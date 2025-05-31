ef9380281ada61baed530e6a2f2254cd68c22a34
Marimar-2025 <mariadelmarsanchez802@gmail.com>
2025-05-30 22:05:36 -0500

    modificacion

--- Procesos.sql
+++ Procesos.sql
@@ -18,51 +18,48 @@ END$$
 DELIMITER ;
 
 
-USE `hotel_reservas`;
-DROP procedure IF EXISTS `realizar_reserva`;
-
-USE `hotel_reservas`;
-DROP procedure IF EXISTS `hotel_reservas`.`realizar_reserva`;
-;
-
 DELIMITER $$
-USE `hotel_reservas`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `realizar_reserva`(
+CREATE PROCEDURE `realizar_reserva`(
     IN id_cli INT,    
     IN id_hab INT,    
     IN fech_entr DATE, 
     IN fech_sal DATE 
 )
 BEGIN
-    DECLARE noches INT(10);
+    DECLARE noches INT;
     DECLARE prec_noche DECIMAL(10,2);
     DECLARE total DECIMAL(10,2);
-    DECLARE dias_anti INT(10);
+    DECLARE dias_anti INT;
     DECLARE descuento DECIMAL(10,2);
     
-    /*dias acticipados*/
-    SET dias_anti = DATEDIFF(p_fech_entr, CURDATE());
+    /*días anticipados*/
+    SET dias_anti = DATEDIFF(fech_entr, NOW());
     
-     /*noches*/
+    /*noches*/
     SET noches = DATEDIFF(fech_sal, fech_entr);
     
     /*precio habitacion*/
-    SELECT prec_noche INTO prec_noche FROM habitacion WHERE id_hab = id_hab;
+	SELECT h.prec_noche INTO prec_noche 
+    FROM habitacion h 
+    WHERE h.id_hab = id_hab
+    LIMIT 1;
     
     /*Cálculo sin descuento*/
     SET total = noches * prec_noche;
     
     /*llama a la función calcular_descuento*/
-    SET descuento = cal_descuento(dias_anti, total);
+    SET descuento = calcular_descuento(dias_anti, total);
     
-    INSERT INTO reserva (fech_entr, fech_sal, noches, total, descuento, id_cli, id_hab)
+    INSERT INTO reserva (fech_entr, fech_sal, noches, tot, descuento, id_cli, id_hab)
     VALUES (fech_entr, fech_sal, noches, total - descuento, descuento, id_cli, id_hab);
     
-	/*estado de la habitación*/
-    UPDATE habitacion SET disponible = FALSE WHERE id_hab = id_hab;
+    /*estado de la habitación*/
+    UPDATE habitacion SET disp = FALSE WHERE habitacion.id_hab = id_hab;
+    
+    SELECT CONCAT('reserva realizada exitosamente.Total:', (total - descuento)) AS resultado;
 END$$
-
 DELIMITER ;
-;
+    
+
 
 
